a82a382a6c572bf5570026765bad715e69e24c9c
rcdavilin <rcdavilin@gmail.com>
2024-05-21 20:41:56 +0200

    Principio de generar informe

--- src/main/java/view/GenerarInforme.java
+++ src/main/java/view/GenerarInforme.java
@@ -1,19 +1,41 @@
 package view;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import controller.MedicoController;
-
+import java.awt.BorderLayout;
 import java.awt.Color;
-import javax.swing.JLabel;
+import java.awt.EventQueue;
 import java.awt.Font;
+import java.io.ByteArrayInputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.border.EmptyBorder;
+
+import org.bson.Document;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import controller.MedicoController;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JsonDataSource;
 
 public class GenerarInforme extends JFrame {
 
@@ -25,6 +47,18 @@ public class GenerarInforme extends JFrame {
 	JLabel lblGenerarInforme;
 	JLabel lblIntroduzcaNombre;
 	MedicoController controllerMedico = new MedicoController();
+	JButton btnCancelar;
+	VentanaPrincipalMedico principal;
+	JButton btnGenerar;
+	String selectedDni, nombrePaciente, apellidosPaciente, nombreMedico, apellidosMedico, fechaNaciemiento, sexo,
+			especialidad, enfermedad, tipo, fechaIngreso;
+	String[] alergenos;
+	String[] medicamentos;
+	String filePath = "C:\\Users\\mamj2\\JaspersoftWorkspace\\MyReports\\Informe";
+	String filejrxml = filePath + ".jrxml";
+	HashMap<String, Object> parametros = new HashMap<>();
+	JasperReport informeEXE;
+	JasperPrint informeGenerado;
 
 	/**
 	 * Launch the application.
@@ -50,7 +84,7 @@ public class GenerarInforme extends JFrame {
 		dniPaciente = controllerMedico.dniPacientes(dni);
 		setBackground(new Color(230, 230, 250));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 531, 391);
+		setBounds(100, 100, 483, 357);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(230, 230, 250));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -77,5 +111,111 @@ public class GenerarInforme extends JFrame {
 		lblGenerarInforme.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblGenerarInforme.setBounds(193, 23, 148, 21);
 		contentPane.add(lblGenerarInforme);
+
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(e -> {
+			principal = new VentanaPrincipalMedico(dni);
+			principal.setVisible(true);
+			dispose();
+		});
+		btnCancelar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnCancelar.setBounds(120, 213, 85, 33);
+		contentPane.add(btnCancelar);
+
+		btnGenerar = new JButton("Generar");
+		btnGenerar.addActionListener(e -> {
+			String filepdf = filePath + ".pdf";
+			selectedDni = comboBoxDniPacientes.getSelectedItem().toString();
+			int option = JOptionPane.showConfirmDialog(null,
+					"¿Desea generar el informe al paciente con DNI " + selectedDni + "?", "Generar informe",
+					JOptionPane.YES_NO_OPTION);
+			if (option == JOptionPane.YES_OPTION) {
+				Optional<Document> paciente = controllerMedico.findByDniPaciente(selectedDni);
+				alergenos = controllerMedico.findAlergenosPaciente(selectedDni);
+				medicamentos = controllerMedico.findMedicamentosPaciente(selectedDni);
+				nombrePaciente = controllerMedico.findNombrePacientePorDni(selectedDni);
+				apellidosPaciente = controllerMedico.findApellidosPacientePorDni(selectedDni);
+				fechaNaciemiento = controllerMedico.findFechaNacimientoPorDni(selectedDni);
+				sexo = controllerMedico.findSexoPorDni(selectedDni);
+				enfermedad = controllerMedico.findEnfermedadIngreso(selectedDni);
+				tipo = controllerMedico.findTipo(selectedDni);
+				fechaIngreso = controllerMedico.findFechaIngreso(selectedDni);
+				nombreMedico = controllerMedico.findNombreMedicoPorDni(dni);
+				apellidosMedico = controllerMedico.findApellidosMedicoPorDni(dni);
+				especialidad = controllerMedico.findEspecialidadPorDni(dni);
+				DateTimeFormatter formateador = DateTimeFormatter.ofPattern("EEEE, d 'de' MMMM 'de' uuuu HH:mm");
+				String fechaFormateada = LocalDateTime.now().format(formateador);
+				parametros.put("Dni", selectedDni);
+				parametros.put("Nombre", nombrePaciente);
+				parametros.put("Apellidos", apellidosPaciente);
+				parametros.put("Fecha_Nacimiento", fechaNaciemiento);
+				parametros.put("Sexo", sexo);
+				parametros.put("Alergenos", String.join(", ", alergenos));
+				parametros.put("Medicamentos", String.join(", ", medicamentos));
+				parametros.put("Enfermedad", enfermedad);
+				parametros.put("Tipo", tipo);
+				parametros.put("Fecha_Ingreso", fechaIngreso);
+				parametros.put("DniMedico", dni);
+				parametros.put("NombreMedico", nombreMedico);
+				parametros.put("ApellidosMedico", apellidosMedico);
+				parametros.put("EspecialidadMedico", especialidad);
+				parametros.put("Fecha_Informe", fechaFormateada);
+
+				try {
+				    // Obtener los documentos de la colección
+				    List<Document> documents = new ArrayList<>();
+				    paciente.ifPresent(documents::add);
+
+				    // Crear el ObjectMapper de Jackson
+				    ObjectMapper mapper = new ObjectMapper();
+				    ArrayNode arrayNode = mapper.createArrayNode();
+
+				    // Convertir cada documento a ObjectNode y añadir al ArrayNode
+				    for (Document doc : documents) {
+				        ObjectNode objectNode = mapper.readValue(doc.toJson(), ObjectNode.class);
+				        arrayNode.add(objectNode);
+				    }
+
+				   
+				    String json = arrayNode.toString();
+
+				    
+				    ByteArrayInputStream jsonDataStream = new ByteArrayInputStream(json.getBytes());
+				    JsonDataSource dataSource = new JsonDataSource(jsonDataStream);
+
+				
+				    JasperReport informeEXE = JasperCompileManager.compileReport(filejrxml);
+				    JasperPrint informeGenerado = JasperFillManager.fillReport(informeEXE, parametros, dataSource);
+
+				   
+				    JDialog progressDialog = new JDialog();
+				    progressDialog.setTitle("Generando Informe");
+				    progressDialog.setModal(true);
+				    progressDialog.setLayout(new BorderLayout());
+				    progressDialog.setSize(300, 100);
+				    progressDialog.setLocationRelativeTo(null); 
+
+				    JProgressBar progressBar = new JProgressBar();
+				    progressBar.setIndeterminate(true); 
+				    progressDialog.add(progressBar, BorderLayout.CENTER);
+
+				    JLabel progressLabel = new JLabel("Generando informe...");
+				    progressDialog.add(progressLabel, BorderLayout.SOUTH);
+				    progressDialog.setVisible(true);
+
+				    Thread.sleep(3000); 
+				    progressDialog.dispose(); 
+				    JasperExportManager.exportReportToPdfFile(informeGenerado, filepdf);
+				    JOptionPane.showMessageDialog(null, "Informe generado con éxito", "Generación de Informe", JOptionPane.INFORMATION_MESSAGE);
+
+				} catch (Exception ex) {
+				    ex.printStackTrace();
+				}
+
+			}
+		});
+		btnGenerar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnGenerar.setBounds(276, 213, 85, 33);
+		contentPane.add(btnGenerar);
 	}
 }
